85445a647457510edb6dd0598f0ead37aff143e0
franců <francupatrik@seznam.cz>
2025-06-05 11:44:34 +0200

    Feature 2 - movie detail 30:45

--- viewer/views.py
+++ viewer/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView, ListView, DetailView
 
 from viewer.models import Movie
 
@@ -6,13 +8,41 @@ from viewer.models import Movie
 def home(request):
     return render(request, 'home.html')
 
+
 def movies(request):
     movies_list = Movie.objects.all()
     context = {'movies': movies_list}
     return render(request, 'movies.html', context)
 
 
-def movie(request, pk): #pk = primary key
+class MoviesView(View):
+    def get(self, request):
+        movies_list = Movie.objects.all()
+        context = {'movies': movies_list}
+        return render(request, 'movies.html', context)
+
+
+class MoviesTemplateView(TemplateView):
+    template_name = 'movies.html'
+    extra_context = {'movies': Movie.objects.all()}
+
+
+class MoviesListView(ListView):
+    template_name = 'movies.html'
+    model = Movie
+    # POZOR, defaultně se objekt poslaný do template jmenuje 'object_list'
+    # buď v template používáme název 'object_list'
+    # nebo můžu dodatečně přejmenovat:
+    context_object_name = 'movies'
+
+
+def movie(request, pk):
     if Movie.objects.filter(id=pk).exists():
         return render(request, 'movie.html', {'movie': Movie.objects.get(id=pk)})
     return render(request, 'home.html')
+
+
+class MovieDetailView(DetailView):
+    template_name = 'movie.html'
+    model = Movie
+    context_object_name = 'movie'
