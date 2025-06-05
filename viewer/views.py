6073cd4355ff5030da038941e506f29dd9cef6f2
franců <francupatrik@seznam.cz>
2025-06-05 14:02:47 +0200

    Delete redundant code

--- viewer/views.py
+++ viewer/views.py
@@ -2,48 +2,66 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView, ListView, DetailView
 
-from viewer.models import Movie
+from viewer.models import Movie, Creator, Country, Genre
 
 
 def home(request):
     return render(request, 'home.html')
 
 
-def movies(request):
-    movies_list = Movie.objects.all()
-    context = {'movies': movies_list}
-    return render(request, 'movies.html', context)
-
-
-class MoviesView(View):
-    def get(self, request):
-        movies_list = Movie.objects.all()
-        context = {'movies': movies_list}
-        return render(request, 'movies.html', context)
-
-
-class MoviesTemplateView(TemplateView):
-    template_name = 'movies.html'
-    extra_context = {'movies': Movie.objects.all()}
-
-
 class MoviesListView(ListView):
     template_name = 'movies.html'
     model = Movie
-    # POZOR, defaultně se objekt poslaný do template jmenuje 'object_list'
-    # buď v template používáme název 'object_list'
-    # nebo můžu dodatečně přejmenovat:
     context_object_name = 'movies'
-
-
-def movie(request, pk):
-    if Movie.objects.filter(id=pk).exists():
-        return render(request, 'movie.html', {'movie': Movie.objects.get(id=pk)})
-    return render(request, 'home.html')
-
 
 
 class MovieDetailView(DetailView):
     template_name = 'movie.html'
     model = Movie
     context_object_name = 'movie'
+
+
+class CreatorsListView(ListView):
+    template_name = 'creators.html'
+    model = Creator
+    context_object_name = 'creators'
+
+
+class ActorsView(View):
+    def get(self, request):
+        creators = Creator.objects.all()
+        actors = []
+        for creator in creators:
+            if creator.acting.exists():
+                actors.append(creator)
+        return render(request, 'actors.html', {'actors': actors})
+
+
+class CreatorDetailView(DetailView):
+    template_name = 'creator.html'
+    model = Creator
+    context_object_name = 'creator'
+
+
+class CountriesListView(ListView):
+    template_name = 'countries.html'
+    model = Country
+    context_object_name = 'countries'
+
+
+class CountryDetailView(DetailView):
+    template_name = 'country.html'
+    model = Country
+    context_object_name = 'country'
+
+
+class GenresListView(ListView):
+    template_name = 'genres.html'
+    model = Genre
+    context_object_name = 'genres'
+
+
+class GenreDetailView(DetailView):
+    template_name = 'genre.html'
+    model = Genre
+    context_object_name = 'genre'
